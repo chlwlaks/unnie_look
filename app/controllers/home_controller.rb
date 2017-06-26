38a9573b04fd8ffeae85aed4b763396b91348b3c
ChoiJiMan <linuspauling@naver.com>
2017-06-26 16:15:40 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,32 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def select1
+  end
+  
+  def select2
+  end
+
+  def select3
+  end
+
+  def select4
+  end
+  
+  def select5
+  end
+  
+  def select6
+  end
+  
+  def result1
+  end
+  
+  def result2
+  end
+
+  def result3
+  end
+  
+end
